84c810981cf3458c59fba430a9ab0c76cb893e68
yvett-codes <yvett.nez@gmail.com>
2024-03-18 12:32:00 -0500

    Configured C++ compiler

--- leetcode/14.LongestCommonPrefix/main14.cpp
+++ leetcode/14.LongestCommonPrefix/main14.cpp
@@ -1,1 +1,18 @@
 // STATUS: NOT STARTED
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}

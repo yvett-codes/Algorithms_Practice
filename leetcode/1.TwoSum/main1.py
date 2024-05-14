a9f0873922b0b5fcd7996ff57162d8dd2fc92ca3
yvett-codes <yvett.nez@gmail.com>
2024-05-14 12:51:02 -0500

    Cleaned up 1.TwoSum solution file to showcase two implementations

--- leetcode/1.TwoSum/main1.py
+++ leetcode/1.TwoSum/main1.py
@@ -1,18 +1,23 @@
 '''
 Status: Complete
 Progress: 63/63 Tests Passing
+2 Implentations Included
 '''
 
 class Solution:
+    # Implementation #1
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        # nums_dict = {i: nums[i] for i in range(len(nums))}
+        nums_dict = {i: nums[i] for i in range(len(nums))}
 
-        # for i in nums_dict.keys():
-        #     for j in nums_dict.keys():
-        #         if i == j:
-        #             continue
-        #         if nums_dict[i] + nums_dict[j] == target:
-        #             return [i, j]
+        for i in nums_dict.keys():
+            for j in nums_dict.keys():
+                if i == j:
+                    continue
+                if nums_dict[i] + nums_dict[j] == target:
+                    return [i, j]
+
+    # Implementation #2
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         i = 0
         j = 1
         while i < len(nums) and j < len(nums):
@@ -23,17 +28,3 @@ class Solution:
                 i += 1
             else:
                 j += 1
-
-solution = Solution()
-
-# # Test Case 1
-nums = [3,2,4]
-target = 6
-result = solution.twoSum(nums, target)
-print(f"Expected: [1,2], Actual: {result}")
-
-# Test Case 2
-nums = [2,7,11,15]
-target = 9
-result = solution.twoSum(nums, target)
-print(f"Expected: [0,1], Actual: {result}")

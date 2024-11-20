4959e52fecb3011e8c234330d96186658ff8d95f
Yvett Jimenez <yvett.nez@gmail.com>
2024-11-20 14:41:46 -0600

    Time: 10 ms (10.92%), Space: 16.8 MB (27.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -20,34 +20,22 @@ class Solution:
             num2 += f"{cur2.val}"
             cur2 = cur2.next
         num2 = int(num2[::-1])
-
-        print(num1, type(num1))
-        print(num2, type(num2))
         
         # revert
         num3 = num1 + num2
-        print(num3, type(num3))
         num3 = str(num3)
         num3 = num3[::-1]
         
         # build linked list
-        # how to loop to create a linked list? create a hash map of the nodes?
-        # l3 = ListNode(num3[0]) if len(num3) < 2 else ListNode(num3[0])
         l3_map = {}
         for i in range(len(num3)):
             l3_map[i] = ListNode(int(num3[i]))
-        print(l3_map)
 
-        l3 = []
         for key, value in l3_map.items():
             key += 1
-            # value.next = l3[key]
-            # value.next = l3[key] if key < len(num3) - 1 else None
             if key < len(num3):
                 value.next = l3_map[key]
             else:
                 value.next = None
-            # l3.append(l3_map[key])
 
-        print(l3_map[0])
         return l3_map[0]

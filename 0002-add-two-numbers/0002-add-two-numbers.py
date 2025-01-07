e1e7e6f1f7003cbe953cc44a9927767265e5d625
Yvett Jimenez <yvett.nez@gmail.com>
2025-01-07 15:53:09 -0600

    Time: 0 ms (100%), Space: 17.9 MB (19.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -8,10 +8,9 @@ class Solution:
         curA = l1
         curB = l2
         l3 = None
-        # dum = ListNode()
         carry = 0
 
-        while curA or curB: # meh
+        while curA or curB:
             curA_val = 0 if not curA else curA.val
             curB_val = 0 if not curB else curB.val
             to_add = curA_val + curB_val + carry
@@ -20,32 +19,23 @@ class Solution:
                 to_add -= 10
                 curC = ListNode(to_add)
                 carry = 1
-                print(f"if statement curC: {curC}")
             else:
                 curC = ListNode(to_add)
                 carry = 0
-                print(f"if statement curC: {curC}")
 
-            print(f"in loop l3: {l3}")
-            print(f"out of conditional curC: {curC}")
             if not l3:
                 l3 = curC
                 prev = curC
-            # prev = curC
             else:
                 prev.next = curC
                 prev = prev.next
-            # curC = curC.next
             
             curA = curA.next if curA else None
             curB = curB.next if curB else None
-            print(f'l3: {l3} END OF LOOP')
 
         curC = curA if curA else curB
         if carry:
             prev.next = ListNode(carry)
-
-        print(l3)
         return l3
 
         

888628479d39ff86bdd063692e7077f333648366
Yvett Jimenez <yvett.nez@gmail.com>
2025-01-09 13:05:54 -0600

    Time: 48 ms (39.75%), Space: 40.2 MB (17.58%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -5,30 +5,32 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dum = ListNode()
-        dum.next = head
-        apple = banana = cherry = dum
-        
-        for i in range(k):
-            cherry = apple
-            apple = apple.next
-        
-        while apple.next:
-            apple = apple.next
-            banana = banana.next
+        '''
+        1. Start at the beginning, iterate `k - 1` times to get a pointer at the left node
+        2. To get the pointer on the right, calculate the length of the linked list, then iterate `length - k` times
+        3. Then swap the two values
+        '''
+        pointL = pointR = head
 
-        # switch
-        if cherry.next == banana.next:
-            return head
-        
-        # adjust the .nexts
-        temp = cherry.next
-        cherry.next = banana.next
-        banana.next = temp
-        
-        # adjust the .next.nexts
-        temp = cherry.next.next
-        cherry.next.next = banana.next.next
-        banana.next.next = temp
+        # Adjust left pointer
+        for i in range(k - 1):
+            pointL = pointL.next
 
-        return dum.next
+        # Find length of input
+        length = 1
+        while pointR.next:
+            pointR = pointR.next
+            length += 1
+        pointR = head
+
+        # Adjust right pointer
+        for i in range(length - k):
+            pointR = pointR.next
+
+        # Swap values
+        rightVal = pointR.val
+        leftVal = pointL.val
+        pointL.val = rightVal
+        pointR.val = leftVal
+
+        return head

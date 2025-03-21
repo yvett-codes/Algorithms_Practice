ed03e7d46a581fd1b28b9639816ae321096ff9f4
Yvett Jimenez <yvett.nez@gmail.com>
2025-03-21 12:42:28 -0500

    Time: 11 ms (13.59%), Space: 17.9 MB (75.37%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.py
@@ -4,10 +4,7 @@ class Solution:
         value = min(nums)
         print(abs_value, value)
         for n in nums:
-            if abs(n) == abs_value and n > value:
-                abs_value = abs(n)
-                value = n
-            elif abs(n) < abs_value:
+            if (abs(n) < abs_value) or (abs(n) == abs_value and n > value):
                 abs_value = abs(n)
                 value = n
         return value

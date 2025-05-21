cb881c8333169106614cee83aacec69bdfabc860
Yvett Jimenez <yvett.nez@gmail.com>
2025-05-21 17:38:10 -0500

    Time: 0 ms (100%), Space: 17.9 MB (60.02%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -3,34 +3,16 @@ class Solution:
         totals = []
 
         for x in operations:
-            if not x.isalpha() and x != '+':
-                    totals.append(int(x))
-
-            if x == '+':
+            if x.strip('-').isdigit():
+                totals.append(int(x))
+            elif x == '+':
                 totals.append(totals[-2] + totals[-1])
             elif x == 'D':
                 totals.append(totals[-1] * 2)
             elif x == 'C':
                 totals.pop()
 
-        return sum (totals)
-
-        # iterate through ops
-        # for char, if char is an int, add that to our stack
-            # totals.append(x)
-        # qualify our chars according to conditions
-        # return the sum of the array
-        '''
-        if x == '+':
-            totals.append(totals[-2] + totals[-1])
-        elif x == 'D':
-            totals.append(totals[-1] * 2)
-        elif x == 'C':
-            totals.pop()
-
-        return sum (totals)
-        '''
-        
+        return sum (totals)        
 
         # Time: O(n)
         # Space: O(n)

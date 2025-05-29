0ec196b11b6f8614a28050fe4f3c35159a5ac7c8
Yvett Jimenez <yvett.nez@gmail.com>
2025-05-29 15:24:19 -0500

    Time: 35 ms (73.52%), Space: 17.9 MB (26.65%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,18 +3,18 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        # n is total number of versions, and n represents the values?
-        # go through each and find the lowest bad, where we experience a cross where it's good, bad
-        # so if n is an integer, how do i iterate through the array?
         l, r = 0, n
         first = n
 
         while l <= r:
             mid = l + (r - l) // 2
-            bad = isBadVersion(mid)
-            if not bad: # too low
+            if not isBadVersion(mid):
                 l = mid + 1
-            else: # qualifies
+            else:
                 first = mid
                 r = mid - 1
+        
         return first
+
+        # Time: O(logn)
+        # Space: O(1)

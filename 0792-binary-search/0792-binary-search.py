ec96a3dbe76db46b22a51ce7938e6502feeb590f
Yvett Jimenez <yvett.nez@gmail.com>
2025-05-29 14:10:31 -0500

    Time: 0 ms (100%), Space: 18.7 MB (45.5%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -3,11 +3,11 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] < target:
-                l = mid + 1
+            mid = l + (r-l) //2
+            if nums[mid] == target:
+                return mid
             elif nums[mid] > target:
                 r = mid - 1
-            elif nums[mid] == target:
-                return mid
+            else:
+                l = mid + 1
         return -1
